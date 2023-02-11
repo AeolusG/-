25f48b12293e1f34d71c61f55a5542b08edd0672
Olga <olga68ko@gmail.com>
2023-02-11 05:49:16 +0300

    Завершила адаптивную верстку

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+function changeCity() {
+  const currentCity = document.querySelector('.city-name');
+  const listOfCities = document.querySelectorAll('.dropdown-item');
+  console.log(listOfCities);
+}
+changeCity();
+
+export default changeCity;
